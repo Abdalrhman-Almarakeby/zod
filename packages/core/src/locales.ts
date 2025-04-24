6ccef12d0bd204625436319baa0043f06fb6be07
Abdalrhman Almarakeby <almarakeby.work@gmail.com>
2025-04-24 11:51:19 +0200

    fix: export German locale

--- packages/core/src/locales.ts
+++ packages/core/src/locales.ts
@@ -1,5 +1,6 @@
 import az from "./locales/az.js";
+import de from "./locales/de.js";
 import en from "./locales/en.js";
 import es from "./locales/es.js";
 
-export { az, es, en };
+export { az, es, en, de };
